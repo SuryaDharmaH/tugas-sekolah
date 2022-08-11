042dda3024bc3ed84847212766eb29526d65ac7c
SuryaDharmaH <suryadharmahalim0@gmail.com>
2022-08-11 16:30:22 +0800

    Baru

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,5 +9,62 @@
 </head>
 <body>
     
+<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+  <div class="flex justify-between md:order-2">
+  <div id="user">
+      <button  onclick="toggleMenu()" type="button" class="flex mr-3 text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" >      
+        <img class="w-12 h-12 rounded-full" src="{{URL::asset('/image/login.jpg')}}" alt="user photo">
+      </button>
+      <!-- Dropdown menu -->
+      <div id="menu" class="hidden bg-white rounded mt-3  shadow dark:bg-gray-700 dark:divide-gray-600">
+        <div class="py-3 px-4 text-sm text-gray-900 dark:text-white">
+        <div class="font-bold font-serif ">Anjas </div>
+        <div class="font-bold font-serif text-sm mt-1">anjas@anjas.com</div>
+      </div>
+        <div class="py-1">
+        <a href="/" class=" block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
+        </div>
+    </div>
+</div>
+<div>
+<div class=" items-center w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+    <ul class="flex flex-col p-4 mt-4  rounded-lg  md:flex-row md:space-x-4 md:mt-0 md:text-sm md:font-medium ">
+      <li>
+        <a href="#" class="block py-3 pr-5 pl-5 text-white bg-blue-700 rounded-md dark:text-white" >Dashboard</a>
+      </li>
+      <li>
+        <a href="#" class="block py-3 pr-5 pl-5 text-white bg-black rounded-md">Tambah Siswa</a>
+      </li>
+    
+    </ul>
+  </div>
+  </div>
+  </div>
+</nav>
+
+
+
+
+  <!-- Javascript Login -->
+  <script>
+        var menu = document.getElementById("menu");
+
+        // open/close the menu when the user clicks on the button
+        function toggleMenu() {
+            if (menu.classList.contains('hidden')) {
+                menu.classList.remove('hidden');
+            } else {
+                menu.classList.add('hidden');
+            }
+        }
+     // close the menu when the user clicks outside of it 
+     window.onclick = function (event) {
+            var dropdownWrapper = document.getElementById('user');
+            if (!dropdownWrapper.contains(event.target) && !menu.classList.contains('hidden')) {
+                menu.classList.add('hidden');
+            }
+        }
+    </script>
+
 </body>
 </html>
