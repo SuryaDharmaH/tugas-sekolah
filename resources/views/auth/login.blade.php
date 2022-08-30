59a7c0d03d70667d61ba8ad00bb519f220a10ff6
SuryaDharmaH <suryadharmahalim0@gmail.com>
2022-08-30 16:24:33 +0800

    Baru

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,6 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login</title>
+  <link rel="shortcut icon" href="{{asset('image/login.jpg')}}">
   @vite('resources/css/app.css')
 </head>
 <body>
